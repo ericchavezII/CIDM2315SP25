8e4cb080a2ff3c9297b3afe0bfd0ff2510b0c2d3
ericchavezII <eachavez1@buffs.wtamu.edu>
2025-02-24 21:00:45 -0600

    Add files via upload

---
+++ HW5.cs
@@ -0,0 +1,67 @@
+﻿
+namespace HW5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num1 = 3;
+            int Num2 = 5;
+            
+
+            
+            int Num3 = 7;
+            int Num4 = 9;
+
+            int Max1 = largestNum(Num1, Num2);
+            int Max2 = largestNum(Num3, Num4);
+
+            int GetLargestTwo = largestNum(Num1 , Num2);
+            Console.WriteLine("the largest out of these two are " + GetLargestTwo);
+
+            int GetLargestFour = largestNum(Max1 , Max2);
+
+            Console.WriteLine("The largest number out of these four is " + GetLargestFour );
+
+            createAccount();
+
+
+           
+        }
+
+        static int largestNum(int a, int b)
+        {
+            return (a > b) ? a : b;
+            
+        }
+        
+        static void createAccount(){
+            Console.Write("enter username : ");
+            string udername = Console.ReadLine();
+
+             Console.Write("enter Password : ");
+            string passWord1 = Console.ReadLine();
+
+             Console.Write("re-enter Password : ");
+            string passWord2 = Console.ReadLine();
+
+             Console.Write("enter birthyear  : ");
+            string birthyear = Console.ReadLine();
+
+            if  (passWord1 == passWord2)
+            {
+                
+                Console.WriteLine("account is created successfully ");
+            }
+            else if (passWord1 != passWord2)
+            {
+                Console.WriteLine("Wrong password");
+            }
+            else
+            {
+                Console.WriteLine("Account could not be created ");
+            }
+        }
+
+    }
+}
